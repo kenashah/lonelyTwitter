81233a949f04a072f1d8371845641d49f95b885c
kenashah <kena@ualberta.ca>
2017-10-11 17:30:09 -0600

    Change 1

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,29 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by kenajshah on 2017-10-11.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
+    public TweetListTest() {
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testAddtweet() {
+        assertTrue(Boolean.FALSE);
+    }
+
+    public void testDelete() {
+        assertTrue(Boolean.FALSE);
+    }
+
+    public void tweetHasTweet() {
+        assertTrue(Boolean.FALSE);
+    }
+
+
+}
+
